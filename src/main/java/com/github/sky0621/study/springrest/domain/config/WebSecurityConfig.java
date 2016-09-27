a5a6907eae3d1b44ee53d375def284c8c359ff61
S.S <ss@ss.com>
2016-09-28 07:13:20 +0900

    security

---
+++ src/main/java/com/github/sky0621/study/springrest/domain/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.github.sky0621.study.springrest.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Configuration
+	@Order(1)
+	public static class ApiWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/api/**");
+			super.configure(http);
+		}
+
+	}
+
+}
